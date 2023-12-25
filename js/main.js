9861aa283cd1c59c952d98b990790cefb1bfaa80
arube <arubensanchezg@gmail.com>
2023-12-25 16:09:04 -0300

    retoques

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
       } else {
         document.querySelector(".error").style.display = "none";
         const data = await response.json();
+        console.log(data)
         document.querySelector(".city").innerHTML = data.name
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C'
         document.querySelector(".humedad").innerHTML = data.main.humidity + '%'
@@ -23,33 +24,40 @@
            case 'Rain':
              weatherIcon.src = "./img/rain.png";
              cuerpo.style.backgroundImage = "url('./img/lluvioso.jpg')";
+             document.querySelector(".descripcion").innerHTML = "lluvia";
              break;
            case 'Thunderstorm': 
              weatherIcon.src = "./img/thunderstorm.png";
              cuerpo.style.backgroundImage = "url('./img/tormenta.jpg')";
+             document.querySelector(".descripcion").innerHTML = "tormentas";
              break;
            case 'Drizzle':
-             weatherIcon.src = "../img/drizzle.png";
-             cuerpo.style.backgroundImage = "url('/img/nublado.jpg')";           
+             weatherIcon.src = "./img/drizzle.png";
+             cuerpo.style.backgroundImage = "url('./img/nublado.jpg')";    
+             document.querySelector(".descripcion").innerHTML = "parcialmente nublado";       
              break;
            case 'Snow':
-             weatherIcon.src = "../img/snow.png";
-             cuerpo.style.backgroundImage = "url('/img/nieve.jpg')";           
+             weatherIcon.src = "./img/snow.png";
+             cuerpo.style.backgroundImage = "url('./img/nieve.jpg')";   
+             document.querySelector(".descripcion").innerHTML = "Nieve";        
              break;
            case 'Mist': 
-             weatherIcon.src = "/img/mist.png";           
+             weatherIcon.src = "./img/mist.png";           
              break;
            case 'Clear':
-             weatherIcon.src = "/img/clear.png"; 
-             cuerpo.style.backgroundImage = "url('/img/despejado.jpg')";                               
+             weatherIcon.src = "./img/clear.png"; 
+             cuerpo.style.backgroundImage = "url('./img/despejado.jpg')";
+             document.querySelector(".descripcion").innerHTML = "Despejado";                               
              break;
              case 'Clouds':
-              weatherIcon.src = "/img/clouds.png";
-              cuerpo.style.backgroundImage = "url('/img/nublado.jpg')";                      
+              weatherIcon.src = "./img/clouds.png";
+              cuerpo.style.backgroundImage = "url('./img/nublado.jpg')";                      
+              document.querySelector(".descripcion").innerHTML = "Nublado";
               break;    
            default:                             
-           cuerpo.style.backgroundImage = "url('/img/earth-8067366_1280.png')";           
-             break;
+              cuerpo.style.backgroundImage = "url('./img/earth-8067366_1280png')";
+              document.querySelector(".descripcion").innerHTML = "";
+              break;
         } 
         document.querySelector(".weather").style.display= "block"
       } 
